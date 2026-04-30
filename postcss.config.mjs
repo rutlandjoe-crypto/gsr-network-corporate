8c65833b4f7186d9aa6f99d966dac6bd1a68f932
Joe Rutland <globalsportsreport@gmail.com>
2026-04-30 14:02:41 -0600

    Fix postcss config and restore styling

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,7 @@
 const config = {
-<<<<<<< HEAD
   plugins: {
     "@tailwindcss/postcss": {},
   },
-=======
-  plugins: ["@tailwindcss/postcss"],
->>>>>>> 314ea8406b1329c98f685f4e8a78f1f70cc41f47
 };
 
 export default config;

e0863bceb54844e7ec6c94a84b5d9a682c94984a
Joe Rutland <globalsportsreport@gmail.com>
2026-04-30 12:51:57 -0600

    Fix next.config.ts corruption

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,7 @@
 import type { NextConfig } from "next";
-import { withBotId } from "botid/next/config";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.md$/,
-      type: "asset/source",
-    });
-
-    return config;
-  },
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "vercel.com",
-        port: "",
-        pathname: "/api/www/avatar/**",
-      },
-    ],
-    dangerouslyAllowSVG: true,
-    contentDispositionType: "attachment",
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
+  reactStrictMode: true,
 };
 
-export default withBotId(nextConfig);
+export default nextConfig;

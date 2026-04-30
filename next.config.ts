751ff9f0e0d32abd9d40ad03335bbb0068399453
Joe Rutland <globalsportsreport@gmail.com>
2026-04-30 12:41:24 -0600

    Fix next config error and update dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,45 +1,29 @@
-<<<<<<< HEAD
 import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-=======
-import type { NextConfig } from 'next'
-import { withBotId } from 'botid/next/config'
+import { withBotId } from "botid/next/config";
 
 const nextConfig: NextConfig = {
   webpack(config) {
     config.module.rules.push({
-      test: /\.md/,
-      type: 'asset/source',
-    })
-    return config
+      test: /\.md$/,
+      type: "asset/source",
+    });
+
+    return config;
   },
-  turbopack: {
-    rules: {
-      '*.md': {
-        loaders: ['raw-loader'],
-        as: '*.js',
-      },
-    },
-  },
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'vercel.com',
-        port: '',
-        pathname: '/api/www/avatar/**',
+        protocol: "https",
+        hostname: "vercel.com",
+        port: "",
+        pathname: "/api/www/avatar/**",
       },
     ],
     dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
+    contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
-}
+};
 
-export default withBotId(nextConfig)
->>>>>>> 314ea8406b1329c98f685f4e8a78f1f70cc41f47
+export default withBotId(nextConfig);
